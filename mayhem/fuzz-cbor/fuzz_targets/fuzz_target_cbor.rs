71572af1109c52425e3d82ec306f4c004a5635fc
Backport Worker <backport-worker@savantenvs.local>
2026-09-16 16:43:34 +0000

    Backport xous-core run 10 @ 8969485cccd89a863e44e05d8962cd9a6b088a8c

---
+++ mayhem/fuzz-cbor/fuzz_targets/fuzz_target_cbor.rs
@@ -0,0 +1,13 @@
+// Unmodified copy of apps/vault/libraries/cbor/fuzz/fuzz_targets/fuzz_target_cbor.rs
+// at this commit.
+#![no_main]
+use std::vec::Vec;
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(value) = cbor::read(data) {
+        let mut result = Vec::new();
+        assert!(cbor::write(value, &mut result));
+        assert_eq!(result, data);
+    };
+});
